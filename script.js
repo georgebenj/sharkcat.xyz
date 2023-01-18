cb807569cf71c4e7ecc92ef306bd3b94089b5efd
George Gosden <gosdeng@gmail.com>
2023-01-18 03:01:59 +0000

    new site

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
 const api_url = "https://api.mcsrvstat.us/2/mc.sharkcat.xyz"
+const on_or_off = document.getElementById("status");
 async function getStatus(){
     const response = await fetch(api_url)
     const data = await response.json();
-    const online_status = data.online
-    if(online_status == true){
-        document.getElementById("online_status").style = "color: green"
-        document.getElementById("online_status").innerHTML = "Online"
+    if(data.online == true){
+        on_or_off.style = "color: green"
+        on_or_off.innerHTML = "Online"
     }
     else{
-        document.getElementById("online_status").style = "color: red"
-        document.getElementById("online_status").innerHTML = "Offline"
+        on_or_off.style = "color: red"
+        on_or_off.innerHTML = "Offline"
     }
-
 }
-
-
 getStatus()
